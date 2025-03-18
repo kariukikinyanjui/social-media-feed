fe6e482242f70335ab1b7b66bb288cecf642391c
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-03-18 20:16:35 +0300

    feat: model represents user-generated content

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from core.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes_count = models.PositiveIntegerField(default=0)
+    comments_count = models.PositiveIntegerField(default=0)
+    shares_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"Post by {self.author.username} at {self.timestamp}"
