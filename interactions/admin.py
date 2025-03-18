b2d2caae1d98513304c5b84b84630ec636e4c0c6
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-03-18 20:37:34 +0300

    feat: register the models in the admin panel for easier management

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Comment, Like, Share
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'text', 'timestamp')
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'timestamp')
+
+
+@admin.register(Share)
+class ShareAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'timestamp')
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'content', 'timestamp', 'likes_count', 'comments_count', 'shares_count')
+    search_fields = ('author__username', 'content')
