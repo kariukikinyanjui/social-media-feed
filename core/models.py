03ea0bcc7bd0b593a62a4f5eaa18dce0b4e89668
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-03-18 20:15:48 +0300

    feat: extend django's User model to include additional fields like

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
