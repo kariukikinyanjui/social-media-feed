f49c94e69adeabeeb761ccf9eddc080458453e45
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-03-18 20:34:45 +0300

    feat: comments are tied to a specific post and user

--- interactions/models.py
+++ interactions/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from core.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+
+    def __str__(self):
+        return f"like by {self.user.username} on {self.post}"
+
+
+class Share(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='shares')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shares')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Share by {self.user.username} of {self.post}"
