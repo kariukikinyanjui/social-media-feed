25a234af1d46f9ae2e4e61e8ea439cf0e13a6e2a
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-03-19 11:57:59 +0300

    feat: added queries to fetch likes and shares

--- config/schema.py
+++ config/schema.py
@@ -1,28 +1,8 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 from core.models import User
-from posts.models imoprt Post
+from posts.models import Post
 from interactions.models import Comment, Like, Share
-
-
-class Query(graphene.ObjectType):
-    all_users = graphene.List(UserType)
-    all_posts = graphene.List(PostType)
-    post_by_id = graphene.Field(PostType, id=graphene.ID(required=True))
-
-    def resolve_all_users(root, info):
-        return User.objects.all()
-
-    def resolve_all_posts(root, info):
-        return Post.objects.all()
-
-    def resolve_post_by_id(root, info, id):
-        try:
-            return Post.objects.get(id=id)
-        except Post.DoesNotExist:
-            return None
-
-schema = graphene.Schema(query=Query)
 
 
 class UserType(DjangoObjectType):
@@ -53,3 +33,31 @@ class ShareType(DjangoObjectType):
     class Meta:
         model = Share
         fields = ('id', 'post', 'user', 'timestamp')
+
+
+class Query(graphene.ObjectType):
+    all_users = graphene.List(UserType)
+    all_posts = graphene.List(PostType)
+    all_likes = graphene.List(LikeType)
+    all_shares = graphene.List(ShareType)
+    post_by_id = graphene.Field(PostType, id=graphene.ID(required=True))
+
+    def resolve_all_users(root, info):
+        return User.objects.all()
+
+    def resolve_all_posts(root, info):
+        return Post.objects.all()
+
+    def resolve_all_likes(root, info):
+        return Like.objects.all()
+
+    def resolve_all_shares(root, info):
+        return Share.objects.all()
+
+    def resolve_post_by_id(root, info, id):
+        try:
+            return Post.objects.get(id=id)
+        except Post.DoesNotExist:
+            return None
+
+schema = graphene.Schema(query=Query)
